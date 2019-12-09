09093615ecea4a2c9f4db099569a85b73b1d9d67
hogan <hongju110@126.com>
2019-12-09 17:41:36 +0800

    init project

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="I love you, zhuhua!You are my angel.I miss you day and night." />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
