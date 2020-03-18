ec67008ceea26a44064402f58529b865d570b3c8
Hogan <hongju110@126.com>
2020-03-18 12:42:21 +0800

    update

---
+++ src/views/Resume/components/Cover.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="cover-box">
+    <div class="cover-wrap">
+      DESIGN YOUR LIFE
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.cover-box {
+  height: 100%;
+  padding: 60px;
+  background: url(~@/assets/resume_bg.jpg) no-repeat;
+  .cover-wrap {
+    height: 100%;
+    padding: 30px;
+    border: 10px solid #96A48B;
+    font-size: 80px;
+    font-weight: bold;
+    color: #000;
+  }
+}
+</style>
--- src/views/Resume/index.vue
+++ src/views/Resume/index.vue
@@ -2,21 +2,31 @@
   <div>
     <full-page :options="options">
       <div class="section">
-        <div class="box1">section1</div>
+        <Cover />
       </div>
       <div class="section">
-        <div class="box2">section2</div>
+        <Info />
       </div>
       <div class="section">
-        <div class="box3">section3</div>
+        <div class="box3">welcome</div>
+      </div>
+      <div class="section">
+        <div class="box3">welcome</div>
       </div>
     </full-page>
   </div>
 </template>
 
 <script>
+import Cover from "./components/Cover.vue"
+import Info from "./components/Info.vue"
+
 export default {
-  name: "",
+  name: "Resume",
+  components: {
+    Cover,
+    Info,
+  },
   data() {
     return {
       options: {
@@ -28,15 +38,10 @@ export default {
         navigation: true,
         // anchors: ['page1', 'page2', 'page3'],
         sectionsColor: [
-          "#41b883",
-          "#ff5f45",
-          "#0798ec",
-          "#fec401",
-          "#1bcee6",
-          "#ee1a59",
-          "#2c3e4f",
-          "#ba5be9",
-          "#b4b8ab"
+          // "#DADAD8",
+          "#ECECEA",
+          "#9CA8B8",
+          "#ECECEA",
         ]
       }
     };
