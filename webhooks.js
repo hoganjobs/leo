d0246817aa596e21f9a36b401311e909502b57b2
hoganjobs <hongju110@126.com>
2021-02-04 17:46:56 +0800

    update

--- webhooks.js
+++ webhooks.js
@@ -27,11 +27,6 @@ http.createServer(function (req, res) {
 handler.on('error', function (err) {
     console.error('Error:', err.message)
 })
-
-handler.on('*', function (event) {
-    console.log('Received *', event);
-    //   run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
-})
  
 handler.on('push', function (event) {
     console.log('Received a push event for %s to %s',
