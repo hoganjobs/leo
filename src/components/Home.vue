8ea2d2fef18c677d76de05742e09d06f75ce0441
Hogan <hongju110@126.com>
2022-01-21 21:47:05 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,8 @@
     <img class="img" alt="logo" src="../assets/logo.jpg" />
     <h1>{{ msg }}</h1>
     <p>Year of the Ox</p>
-    <h5>MIT Licensed | Copyright © 2021-present hoganjobs</h5>
+    <h5>MIT Licensed | Copyright © 2022-present hoganjobs</h5>
+    <h5><a class="beian" href="https://beian.miit.gov.cn/" target="_blank">粤ICP备20054772号</a></h5>
   </div>
 </template>
 
@@ -20,5 +21,10 @@ export default {
 <style scoped>
 img {
   border-radius: 50%;
+}
+.beian {
+  text-decoration: none;
+  font-size: 12px;
+  color: #2c3e50;
 }
 </style>
