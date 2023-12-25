1b5e08b6e71628717fe84fa7d3bd29eb78654156
wanghongju <wanghongju@xuelipai.cn>
2023-12-25 11:23:36 +0800

    chore: update to vue3.x

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
-<template>
-  <div id="app">
-    <Home msg="Welcome to Hoganjobs Page"/>
-  </div>
-</template>
-
-<script>
-import Home from './components/Home.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Home
-  }
-}
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface IProps {
+  msg: string
+}
+
+defineProps<IProps>()
+</script>
+
+<template>
+  <div class="home">
+    <img class="img" alt="logo" src="../assets/logo.jpg" />
+    <h1>Welcome to Hoganjobs Page</h1>
+    <p>Year of the Rabbit</p>
+    <h5>MIT Licensed | Copyright © 2020-present hoganjobs</h5>
+    <h5><a class="beian" href="https://beian.miit.gov.cn/" target="_blank">粤ICP备20054772号</a></h5>
+  </div>
+</template>
